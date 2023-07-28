afe4c8bb87fc1d3fd5450afb08b4ca67824999ea
Lumen Yang <lumen.yang@lumeny.io>
2023-07-29 00:06:09 +0200

    implemented the task subcommand

--- lumeny/cli.py
+++ lumeny/cli.py
@@ -3,10 +3,12 @@ import os
 import questionary as q
 import click
 from prompt_toolkit.lexers import PygmentsLexer
-from lumeny.utils import InputLexer, print_green
+from lumeny.utils import InputLexer
 
 # from lumen.tui import launch_tui
-from lumeny.calendar import generate_command_with_gpt4, repeat_learn
+# from lumeny.calendar import generate_command_with_gpt4, repeat_learn
+from lumeny.task_picker import random_task_picker, available_sections
+from termcolor import colored
 
 
 @click.group()
@@ -45,6 +47,8 @@ def add():
 
         return
 
+    return
+
 
 @click.command()
 @click.option("-t", "--topic", type=str, help="Specify the topic.")
@@ -64,7 +68,6 @@ def add():
     help="Specify a start date as an integer.",
 )
 def repeat(topic, repeat_days, start_date):
-
     format_str = "%d-%m-%Y"
 
     command: str = repeat_learn(topic, repeat_days, start_date)
@@ -93,10 +96,12 @@ def will(input):
     if q.confirm(f"Execute: {command}", qmark="?").ask():
         try:
             os.system(command)
-            print(f"\033[1;32mEvent Added!\033[0m")
+            print(colored("Event Added!", "green"))
         except Exception as e:
             print(e)
             print("Error executing command")
+
+    return
 
 
 @click.command()
@@ -109,10 +114,37 @@ def show():
     # launch_tui()
 
 
+@click.command()
+@click.option(
+    "-a", "--all", is_flag=True, default=False, help="Give me a task without any pick"
+)
+def task(all):
+    """
+    Choose a random task to do.
+    """
+    section_list = available_sections()
+
+    section_list.append("All")
+
+    # choose a section
+    if not all:
+        section = q.select(
+            "Choose a section",
+            choices=section_list,
+            qmark="?",
+        ).ask()
+    else:
+        section = "All"
+
+    # choose a task
+    random_task_picker(section_code = section if section != "All" else None)
+
+
 cli.add_command(will)
 cli.add_command(show)
 cli.add_command(add)
 cli.add_command(repeat)
+cli.add_command(task)
 
 if __name__ == "__main__":
     cli()
